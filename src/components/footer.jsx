57873c0d5a5503ac77493261ab582c7e4bbe054e
nurma <denialperson5@gmail.com>
2026-05-17 02:14:19 +0700

    frontend-bookstore

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -10,7 +10,7 @@ export default function Footer() {
 
             <div>
               <h2 className="text-2xl font-bold text-gray-900 dark:text-white">
-                BookSales
+                BookStore
               </h2>
 
               <p className="mt-4 text-gray-500 dark:text-gray-400 leading-7">
